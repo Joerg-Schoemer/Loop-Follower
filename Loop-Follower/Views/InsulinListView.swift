c87fa32c257aa8dc4c2e4644ba176a55fa45fa8f
Jörg Schömer <joerg@joerg-schoemer.de>
2023-11-10 00:29:11 +0100

    sum selection in InsulinListView

--- Loop-Follower/Views/InsulinListView.swift
+++ Loop-Follower/Views/InsulinListView.swift
@@ -11,22 +11,43 @@ struct InsulinListView: View {
     
     @EnvironmentObject var modelData : ModelData
 
+    @State private var multiSelection = Set<String>()
+
+    @State private var editMode : EditMode = .inactive
+
     var body: some View {
         VStack{
-            List(modelData.insulin, id: \.id) { insulin in
+            List(modelData.insulin, id: \.id, selection: $multiSelection) { insulin in
                 InsulinItem(insulin: insulin)
             }
+            .toolbar { EditButton() }
+            .environment(\.editMode, $editMode)
+            .onChange(of: editMode) { oldValue, newValue in
+                if newValue == .active {
+                    multiSelection.formUnion(modelData.insulin.map{ cb in cb.id })
+                } else {
+                    multiSelection.removeAll()
+                }
+            }
+
             Spacer()
+
             Label(
                 sum.formatted(.measurement(width: .abbreviated, numberFormatStyle: .number.precision(.fractionLength(2)))),
                 systemImage: "sum"
-            ).font(.headline)
+            )
+            .font(.headline)
         }
         .navigationBarTitle("Insulin")
     }
-    
+
     var sum : Measurement<UnitInsulin> {
-        return Measurement<UnitInsulin>(value: modelData.insulin.map{ $0.insulin }.reduce(0, +), unit: .insulin)
+        var selectedItems = modelData.insulin
+        if editMode == .active {
+            selectedItems = modelData.insulin.filter { insulin in multiSelection.contains(insulin.id) }
+        }
+
+        return Measurement<UnitInsulin>(value: selectedItems.map{ $0.insulin }.reduce(0, +), unit: .insulin)
     }
 }
 
